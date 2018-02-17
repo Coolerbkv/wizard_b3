e57d0d267ce8d5cf8e0c2ea414d02c0f5a6dee82
gpugachev <36511820+gpugachev@users.noreply.github.com>
2018-02-17 13:46:22 +0300

    Add FormSet

---
+++ InsWebApp/FormsModel/Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsWebApp.FormsModel
+{
+    public sealed class Form
+    {
+        private Dictionary<string, FormControl> _controls;
+        public Dictionary<string, FormControl> Controls
+        {
+            get { return _controls ?? (_controls = new Dictionary<string, FormControl>()); }
+        }
+
+        internal Form()
+        {
+            
+        }
+
+        internal void AddControl(string formId, FormControl ctrl)
+        {
+            _controls.Add(formId, ctrl);
+        }
+    }
+}
---
+++ InsWebApp/FormsModel/FormStorage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XStar.Support;
+
+namespace InsWebApp.FormsModel
+{
+    public sealed class FormStorage
+    {
+        private static object _bundles_lock_sync = new object();
+        public static FormStorage Instance
+        {
+            get
+            {
+                return Singleton<FormStorage>.Instance;
+            }
+        }
+
+        private CacheCollection _formSets;
+
+        private CacheCollection FormSets
+        {
+            get
+            {
+                if (_formSets == null)
+                {
+                    int count;
+                    //if (!int.TryParse(ProjectSettings.GetSettingValue("StatesCached"), out count))
+                    count = 150;
+
+                    _formSets = new CacheCollection(count);
+                }
+
+                return _formSets;
+            }
+        }
+
+        public FormSet GetFormSet(string activeProductClass, string productId, string state)
+        {
+            FormSet formSet;
+            lock (_bundles_lock_sync)
+            {
+                string fullFormSetId = (productId + "-" + state);
+                formSet = (FormSet)FormSets[fullFormSetId];
+                if (formSet == null)
+                {
+                    // try to load
+                    formSet = CreateFormSet(activeProductClass, productId, state);
+                    FormSets.Add(fullFormSetId, formSet);
+                }
+            }
+            return formSet;
+        }
+
+
+        private FormSet CreateFormSet(string activeProductClass, string productId, string state)
+        {
+            return FormSetCreator.Instance.CreateFormSet(activeProductClass, productId, state);
+        }
+    }
+}
