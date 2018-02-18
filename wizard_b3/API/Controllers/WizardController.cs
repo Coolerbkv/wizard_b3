5a8f29f96900c59a5a1eec1d91b8b7046d7a7ffc
gpugachev <36511820+gpugachev@users.noreply.github.com>
2018-02-18 13:27:10 +0300

    Send correct model

--- wizard_b3/API/Controllers/WizardController.cs
+++ wizard_b3/API/Controllers/WizardController.cs
@@ -5,16 +5,26 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using InsWebApp.FormsModel;
+using wizard_b3.Models;
 
 namespace wizard_b3.API.Controllers
 {
     public class WizardController : ApiController
     {
         [HttpGet]
-        [Route("~/api/Wizard/getAllForms")]
+        [Route("~/api/wizard/getAllForms")]
         public object GetAllForms()
         {
-            return FormStorage.Instance.GetFormSet("", "AK");
+            var formSet = FormStorage.Instance.GetFormSet("", "AK");
+
+            var formsModelList = formSet.Forms.Values.Select(form => new FormModel()
+            {
+                Id = form.Id,
+                Name = form.Name,
+                Controls = form.Controls.Select(c => c.Value).ToList()
+            }).ToList();
+
+            return formsModelList;
         }
     }
 }
---
+++ wizard_b3/Models/FormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InsWebApp.FormsModel;
+
+namespace wizard_b3.Models
+{
+    public class FormModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<FormControl> Controls = new List<FormControl>(); 
+    }
+}
