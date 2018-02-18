1128633197950daeb28db5b070435775777e2e43
gpugachev <36511820+gpugachev@users.noreply.github.com>
2018-02-18 14:06:58 +0300

    Fix enum displaying, and make attr lower case

--- wizard_b3/Global.asax.cs
+++ wizard_b3/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Newtonsoft.Json.Serialization;
 using wizard_b3.App_Start;
 
 namespace wizard_b3
@@ -17,6 +18,10 @@ namespace wizard_b3
             GlobalConfiguration.Configure(WebApiConfig.Register);
             //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            HttpConfiguration config = GlobalConfiguration.Configuration;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
         }
     }
 }
