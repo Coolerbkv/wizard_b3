7f13ab236f6230dbd060fef0c0fd88d42317f483
Kirill Bodyako <kirillb@fptrade.net>
2018-02-23 11:07:30 +0300

    fix

--- wizard-b3-web/src/polyfills.ts
+++ wizard-b3-web/src/polyfills.ts
@@ -63,5 +63,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
+import 'hammerjs';
 import 'web-animations-js/web-animations.min';
